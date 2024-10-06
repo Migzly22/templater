086953f310059ed4f1104d94d04779287489de78
Rolly Migriño <rolly.migrino02@gmail.com>
2024-10-06 19:22:18 +0800

    Git Push

--- git-push.sh
+++ git-push.sh
@@ -1,11 +1,50 @@
 #!/bin/bash
 
 #Check if the parameter is greater than 2 or equal
-if [ $# -lt 1 ]; then
+COMMIT=""
+FILES=""
+REPO=""
+
+usage() {
+    echo "Usage: $0 --commit <commit_msg> [--files <files_value>] [--repo <repo_value>]"
+    exit 1
+}
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --commit)  
+            shift 
+            COMMIT="$1"  
+            shift 
+            ;;
+        --files)  
+            shift  
+            FILES="$1"  
+            shift  
+            ;;
+        --repo)  
+            shift  
+            REPO="$1"  
+            shift  
+            ;;
+        *)  
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
+
+if [ -z "$FILES" ]; then
+  FILES="."
+fi
+if [ -z "$REPO" ]; then
+  REPO="master"
+fi
+if [ -z "$COMMIT" ]; then
   echo "Commit Message is Needed"
   exit 1
 fi
 
-git add .
-git commit -m "$1"
-git push
+git add $FILES
+git commit -m "$COMMIT"
+git push origin $REPO
